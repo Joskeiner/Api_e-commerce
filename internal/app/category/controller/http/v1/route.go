b6cb40f55a8a2d17244dab04086ae7b4a2a4cd55
joskeiner simosa urdaneta <joskeiner.simosa9@gmail.com>
2024-02-22 22:37:21 -0300

    add middleware DELETE and PUT

--- internal/app/category/controller/http/v1/route.go
+++ internal/app/category/controller/http/v1/route.go
@@ -12,6 +12,6 @@ func (cc *CategoryControllerV1) InitRoutes(routeV1 fiber.Router, token token.Tok
 	routeV1.Get("/", cc.GetAll)
 	routeV1.Post("/", middleware.AuthMiddleware(token), middleware.AdminMiddleware(), cc.Create)
 	routeV1.Get("/:id", middleware.AuthMiddleware(token), cc.GetByID)
-	routeV1.Put("/:id", middleware.AuthMiddleware(token), cc.Update)
-	routeV1.Delete("/:id", middleware.AuthMiddleware(token), cc.Delete)
+	routeV1.Put("/:id", middleware.AuthMiddleware(token), middleware.AdminMiddleware(), cc.Update)
+	routeV1.Delete("/:id", middleware.AuthMiddleware(token), middleware.AdminMiddleware(), cc.Delete)
 }
